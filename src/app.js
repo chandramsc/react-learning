bff7a3516e9ea0a3b8b063d2f4c8e3722fc16bde
chandrasekaran <chandrasekaran@tartlabs.com>
2018-04-29 21:44:41 +0530

    Creating a React Component

--- src/app.js
+++ src/app.js
@@ -1,79 +1,47 @@
-console.log('App.js is running!');
-
-const app = {
-    title: 'React Learning',
-    subtitile: 'put your text',
-    options: []
-}
-
-// JSX - JavaScript XML
-// 1. Create render function that renders the new jsx
-// 2. Call it right away
-// 3. Call it after options array added to
-
-const onSubmitForm = (e) => {
-  e.preventDefault();
-  const option = e.target.elements.option.value;
-
-  if(option) {
-    app.options.push(option);
-    e.target.elements.option.value = '';
-    render();
-  }
-};
-
-// Create "Remove All" button above list
-// on click -> wipe the array -> rerender
-
-const onRemoveAll = () => {
-  app.options = [];
-  render();
-};
-
-const onMakeDecision = () => {
-  const randomNum = Math.floor(Math.random() * app.options.length);
-  const option = app.options[randomNum];
-  alert(option);
-  // console.log(randomNum);
-};
-
-const appRoot = document.getElementById('app');
-
-// const numbers = [51,100,151];
-
-const render = () => {
-  const template = (
+class Header extends React.Component {
+  render() {
+    return (
       <div>
-          <h2>{app.title}</h2>
-          {app.subtitile && <p>{app.subtitile}</p>}
-          <p>{app.options.length > 0 ? 'Here are in your options' : 'No Options'}</p>
-          <p>{app.options.length}</p>
-          <button disabled={app.options.length === 0} onClick={onMakeDecision}>What should i do?</button>
-          <button onClick={onRemoveAll}>Remove All</button>
-          {
-           /*numbers.map((number) => {
-              return <p key={number}>Numbers: {number}</p>;
-            })*/
-          }
-          <ol>
-          {/* map over app.options getting back an array of list (set key and text)  */}
-          {
-            app.options.map((option) => <li key={option}>{option}</li>)
-          }
-          </ol>
-          <form onSubmit={onSubmitForm}>
-            <input type="text" name="option" />
-            <button>Add Options</button>
-          </form>
+        <h1>Indecision</h1>
+        <h2>Put your life in the hands of a computer</h2>
       </div>
-  );
-  
-  ReactDOM.render(template, appRoot);
+    );
+  }
 }
 
-render();
+class Action extends React.Component {
+  render() {
+    return (
+      <div>What should I do?</div>
+    );
+  }
+}
 
+// Options -> Options component here
+class Options extends React.Component {
+  render() {
+    return(
+      <div>Option Componets here</div>
+    );
+  }
+}
 
+// AddOption -> AddOption component here
+class AddOption extends React.Component {
+  render() {
+    return (
+      <div>AddOption component here</div>
+    );
+  }
+}
 
+const jsx = (
+  <div>
+    <Header/>
+    <Action/>
+    <Options/>
+    <AddOption/>
+  </div>
+);
 
-
+ReactDOM.render(jsx, document.getElementById('app'));
