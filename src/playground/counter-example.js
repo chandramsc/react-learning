429b0040f01d2c19b2bbd305fe9d8bf2ea21fbc2
chandrasekaran <chandrasekaran@tartlabs.com>
2018-05-13 11:21:46 +0530

    Adding State to Counter App : Part 1 & 2

--- src/playground/counter-example.js
+++ src/playground/counter-example.js
@@ -1,44 +1,102 @@
-// Events and Attributes & Manual Data Binding
+class Counter extends React.Component {
 
-// 1. Make button "+1" - setup addOne function and register - log "addOne".
-// 2. Make button "-1" - setup minusOne function and register - log "minusOne".
-// 3. Make reset button "reset" - setup reset function - log "reset"
+  constructor(props) {
+    super(props);
+    this.handleAddOne = this.handleAddOne.bind(this);
+    this.handleMinusOne = this.handleMinusOne.bind(this);
+    this.handleReset = this.handleReset.bind(this);
+    this.state = {
+      count : 0
+    }
+  }
+  
+  handleAddOne() {
+    this.setState((prevState) => {
+      return {
+        count : prevState.count + 1
+      };
+    });
+  }
 
-let count = 0;
+  handleMinusOne() {
 
-const addOne = () => {
-  // Add 1 from count - rerender
-  count++;
-  renderCounterApp();
-};
+    // Call this.setState decrement the count by 1
+    this.setState((prevState) => {
+      return {
+        count : prevState.count - 1
+      };
+    });
+  }
+
+  handleReset() {
+    this.setState(() => {
+      return {
+        count: 0
+      };
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>Count : {this.state.count}</h1>
+        <button onClick={this.handleAddOne}>+1</button>
+        <button onClick={this.handleMinusOne}>-1</button>
+        <button onClick={this.handleReset}>reset</button>
+      </div>
+    );
+  }
+}
+
+// Create three methods : handleAddOne, handleMinusOne, handleReset
+// Use console.log to print method name
+// Wire up onClick & bind in the constructor
 
 
-const minusOne = () => {
-  // Subtract 1 from count - rerender
-  count--;
-  renderCounterApp();
-};
+ReactDOM.render(<Counter/>, document.getElementById('app'));
 
 
-const reset = () => {
-  // Set count to 0 a rerender
-  count = 0;
-  renderCounterApp();
-};
+// // Events and Attributes & Manual Data Binding
 
-const appRoot = document.getElementById('app');
+// // 1. Make button "+1" - setup addOne function and register - log "addOne".
+// // 2. Make button "-1" - setup minusOne function and register - log "minusOne".
+// // 3. Make reset button "reset" - setup reset function - log "reset"
 
-const renderCounterApp = () => {
-  const templateTwo = (
-    <div>
-      <h1>Count {count}</h1>
-      <button onClick={addOne}>+1</button>
-      <button onClick={minusOne}>-1</button>
-      <button onClick={reset}>reset</button>
-    </div>
-  );
+// let count = 0;
 
-  ReactDOM.render(templateTwo, appRoot);
-};
+// const addOne = () => {
+//   // Add 1 from count - rerender
+//   count++;
+//   renderCounterApp();
+// };
 
-renderCounterApp();
+
+// const minusOne = () => {
+//   // Subtract 1 from count - rerender
+//   count--;
+//   renderCounterApp();
+// };
+
+
+// const reset = () => {
+//   // Set count to 0 a rerender
+//   count = 0;
+//   renderCounterApp();
+// };
+
+// const appRoot = document.getElementById('app');
+
+// const renderCounterApp = () => {
+//   const templateTwo = (
+//     <div>
+//       <h1>Count {count}</h1>
+//       <button onClick={addOne}>+1</button>
+//       <button onClick={minusOne}>-1</button>
+//       <button onClick={reset}>reset</button>
+//     </div>
+//   );
+
+//   ReactDOM.render(templateTwo, appRoot);
+// };
+
+// renderCounterApp();
