e1bd87fdf418f8851da4fa10e4bfd1ef1d0cf29b
chandrasekaran <chandrasekaran@tartlabs.com>
2018-06-19 07:53:32 +0530

    Section 7 : Using a Third-Party Component, Passing Children to Component

--- src/app.js
+++ src/app.js
@@ -2,36 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import IndecisionApp from './components/IndecisionApp';
 
-ReactDOM.render(<IndecisionApp />, document.getElementById('app'));
+const Layout = (props) => {
+    return (
+        <div>
+            <h1>Header</h1>
+            {props.children}
+            <p>This is my content</p>
+        </div>
+    );
+};
 
+ReactDOM.render(<Layout><p>chandra</p></Layout>, document.getElementById('app'));
 
-// Old Syntax :
-
-class OldSyntax {
-    constructor() {
-        this.name = 'Chandra';
-        this.getGreeting = this.getGreeting.bind(this);
-    }
-    getGreeting() {
-        return `Hi. My name is ${this.name}.`;
-    }
-}
-
-const oldSyntax = new OldSyntax();
-const getGreeting = oldSyntax.getGreeting;
-console.log(oldSyntax.name);
-console.log(getGreeting());
-
-// New Syntax 
-
-class NewSyntax {
-    name = 'Chandra';
-    getGreeting = () => {
-        return `Hi, My name is ${this.name}`;
-    }
-}
-
-const newSyntax = new NewSyntax();
-const newGetGreeting = newSyntax.getGreeting;
-console.log(newSyntax.name);
-console.log(newGetGreeting());
