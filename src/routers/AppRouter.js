d2e69b3921905bf43b65eeee6abf5c0c829c78e4
chandrasekaran <chandrasekaran@tartlabs.com>
2018-06-24 06:35:10 +0530

    Build It: Router for Portfolio Site

--- src/routers/AppRouter.js
+++ src/routers/AppRouter.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import { BrowserRouter, Route, Switch, Link, NavLink } from 'react-router-dom';
-import ExpenseDashboardPage from '../components/ExpenseDashboardPage';
-import AddDashboardPage from '../components/AddDashboardPage';
-import EditDashboardPage from '../components/EditDashboardPage';
-import HelpPage from '../components/HelpPage';
+import HomePage from '../components/HomePage';
+import ContactPage from '../components/ContactPage';
+import PortfolioPage from '../components/PortfolioPage';
+import PortfolioItemPage from '../components/PortfolioItemPage';
 import NotFountPage from '../components/NotFountPage';
 import Header from '../components/Header';
 
@@ -12,10 +12,10 @@ const AppRouter = () => (
         <div>
             <Header />
             <Switch>
-                <Route path="/" component={ExpenseDashboardPage} exact={true}/>
-                <Route path="/create" component={AddDashboardPage} />
-                <Route path="/edit/:id" component={EditDashboardPage} />
-                <Route path="/help" component={HelpPage} />
+                <Route path="/" component={HomePage} exact={true} />
+                <Route path="/portfolio" component={PortfolioPage} exact={true} />
+                <Route path="/portfolio/:id" component={PortfolioItemPage} />
+                <Route path="/contact" component={ContactPage} />
                 <Route component={NotFountPage} />
             </Switch>
         </div>
