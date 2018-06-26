e96d0718b7d3669e26d9ffbb7d7bbcb368aa6ce3
chandrasekaran <chandrasekaran@tartlabs.com>
2018-06-26 08:42:50 +0530

    Subscribing and Dynamic Actions

--- src/playground/redux-101.js
+++ src/playground/redux-101.js
@@ -1,42 +1,59 @@
 import { createStore } from 'redux';
 
-const store = createStore((state = { count:0 }, action) => {
-    // return state;
+const store = createStore((state = { count: 0 }, action) => {
     switch (action.type) {
         case 'INCREMENT':
+            const incrementBy = typeof action.incrementBy === 'number' ? action.incrementBy : 1;
             return {
-                count: state.count + 1
-            }
-        case 'DECREMENT' :
+                count: state.count + incrementBy
+            };
+        case 'DECREMENT':
+            const decrementBy = typeof action.decrementBy === 'number' ? action.decrementBy : 1;
             return {
-                count: state.count - 1
-            }
-        case 'RESET' :
+                count: state.count - decrementBy
+            };
+        case 'SET':
+            return {
+                count: action.count
+            };
+        case 'RESET':
             return {
                 count: 0
-            }
-    
+            };
         default:
             return state;
     }
-}); 
+});
 
 console.log(store.getState());
 
-// Actions - than an object that gets sent to the store
+const unsubscribe = store.subscribe(() => {
+    console.log(store.getState());
+});
 
-// I'd like to increment the count 
+store.dispatch({
+    type: 'INCREMENT',
+    incrementBy: 5
+});
+
 store.dispatch({
     type: 'INCREMENT'
+});
+
+store.dispatch({
+    type: 'RESET'
 });
 
 store.dispatch({
     type: 'DECREMENT'
 });
 
-// I'd like to reset the count to zero
 store.dispatch({
-    type: 'RESET'
+    type: 'DECREMENT',
+    decrementBy: 10
 });
 
-console.log(store.getState());
+store.dispatch({
+    type: 'SET',
+    count: 101
+});
